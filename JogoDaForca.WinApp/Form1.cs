8573528b6e94e939e945c87a3c35bee49bcb1e75
Davi Borges <davifelipe_300@hotmail.com>
2023-05-11 09:47:45 -0300

    configura o botao de gerar palavra aleatoria e os clicks nos botoes do alfabeto compara com a palavra gerada

--- JogoDaForca.WinApp/Form1.cs
+++ JogoDaForca.WinApp/Form1.cs
@@ -1,14 +1,82 @@
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+
 namespace JogoDaForca.WinApp
 {
     public partial class Form1 : Form
     {
+        JogoDaForca jogoDaForca= new JogoDaForca();
         public Form1()
         {
             InitializeComponent();
             btnGerarFrutaAleatoria.Click += GerarFrutaAleatoria;
+            btnA.Click += ComparaLetraChuteComLetraFruta;
+            btnB.Click += ComparaLetraChuteComLetraFruta;
+            btnC.Click += ComparaLetraChuteComLetraFruta;
+            btnD.Click += ComparaLetraChuteComLetraFruta;
+            btnE.Click += ComparaLetraChuteComLetraFruta;
+            btnF.Click += ComparaLetraChuteComLetraFruta;
+            btnH.Click += ComparaLetraChuteComLetraFruta;
+            btnI.Click += ComparaLetraChuteComLetraFruta;
+            btnJ.Click += ComparaLetraChuteComLetraFruta;
+            btnK.Click += ComparaLetraChuteComLetraFruta;
+            btnL.Click += ComparaLetraChuteComLetraFruta;
+            btnM.Click += ComparaLetraChuteComLetraFruta;
+            btnN.Click += ComparaLetraChuteComLetraFruta;
+            btnO.Click += ComparaLetraChuteComLetraFruta;
+            btnP.Click += ComparaLetraChuteComLetraFruta;
+            btnQ.Click += ComparaLetraChuteComLetraFruta;
+            btnR.Click += ComparaLetraChuteComLetraFruta;
+            btnS.Click += ComparaLetraChuteComLetraFruta;
+            btnT.Click += ComparaLetraChuteComLetraFruta;
+            btnU.Click += ComparaLetraChuteComLetraFruta;
+            btnV.Click += ComparaLetraChuteComLetraFruta;
+            btnX.Click += ComparaLetraChuteComLetraFruta;
+            btnZ.Click += ComparaLetraChuteComLetraFruta;
+            btnW.Click += ComparaLetraChuteComLetraFruta;
+            btnY.Click += ComparaLetraChuteComLetraFruta;
+        }
+
+        private void ComparaLetraChuteComLetraFruta(object? sender, EventArgs e)
+        {
+            Button btnClicado = (Button)sender;
+            //laco do tamanho do array da fruta
+            for (int i = 0; i < jogoDaForca.frutaAdivinhada.Length; i++)
+            {
+                char btn = Convert.ToChar(btnClicado.Text);
+                //compara o chute com um caractere do array na posicao do indice no for
+                if (btn == jogoDaForca.frutaAleatoria[i])
+                {
+                    jogoDaForca.frutaAdivinhada[i] = btn;
+                    //frutaAdivinhada[i] = chute;
+                    //frutaFoiEncontrada = true;
+                }
+            }
+            string test = new string(jogoDaForca.frutaAdivinhada);
+            TextBox frutaConvertidaEmTextBox = new TextBox(); 
+            frutaConvertidaEmTextBox.Text = test;
+            txtFrutaAleatoria.Text = frutaConvertidaEmTextBox.Text;
+            //if (frutaFoiEncontrada == false)
+            //{
+            //    tentativas++;
+            //}
         }
 
         private void GerarFrutaAleatoria(object? sender, EventArgs e)
+        {
+            GeraFrutaUmaAleatoria();
+            TransformarPalavraEmTracinho();
+            AdicionaFrutaAleatoriaNoTextBox();
+        }
+
+        private void AdicionaFrutaAleatoriaNoTextBox()
+        {
+            string fruntaConvertitaEmTrancosString = new string(jogoDaForca.frutaAdivinhada);
+            TextBox frutaConvertidaEmTextBox = new TextBox();
+            frutaConvertidaEmTextBox.Text = fruntaConvertitaEmTrancosString;
+            txtFrutaAleatoria.Text = frutaConvertidaEmTextBox.Text;
+        }
+
+        private void GeraFrutaUmaAleatoria()
         {
             string[] frutas =
             {
@@ -44,20 +112,16 @@ namespace JogoDaForca.WinApp
                     "UVAIA"
                 };
             Random random = new Random();
-            string frutaAleatoria = frutas[random.Next(0, frutas.Length)];
-            char[] frutaAdivinhada = new char[frutaAleatoria.Length];
+            jogoDaForca.frutaAleatoria = frutas[random.Next(0, frutas.Length)];
+        }
 
-            #region transformarPalavraEmTracinho
-            for (int i = 0; i < frutaAdivinhada.Length; i++)
+        private void TransformarPalavraEmTracinho()
+        {
+            jogoDaForca.frutaAdivinhada = new char[jogoDaForca.frutaAleatoria.Length];
+            for (int i = 0; i < jogoDaForca.frutaAdivinhada.Length; i++)
             {
-                frutaAdivinhada[i] = '_';
+                jogoDaForca.frutaAdivinhada[i] = '_';
             }
-
-            string frutaAdivinhadaString = new string(frutaAdivinhada);
-            TextBox textBox = new TextBox();
-            textBox.Text = frutaAdivinhadaString;
-            txtFrutaAleatoria.Text = textBox.Text;
-            #endregion
         }
     }
 }
---
+++ JogoDaForca.WinApp/JogoDaForca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoDaForca.WinApp
+{
+    internal class JogoDaForca
+    {
+        public string[] frutas;
+        public string frutaAleatoria;
+        public char[] frutaAdivinhada;
+    }
+}
